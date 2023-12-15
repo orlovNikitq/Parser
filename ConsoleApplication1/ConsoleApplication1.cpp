12c50c22f46b83d4bd542ba59419c4c758f6fdf8
orlovNikitq <130779328+orlovNikitq@users.noreply.github.com>
2023-12-15 12:54:21 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector> 
+#include <nlohmann/json.hpp>
+using namespace std;
+using json = nlohmann::json;
+
+struct Person {
+    string name;
+    string surname;
+};
+
+int main() {
+    ifstream file("qwe.json");
+
+    json json_data;
+    file >> json_data;
+    vector<Person> people;
+
+    for (const auto& entry : json_data) {
+        Person person;
+        person.name = entry["name"];
+        person.surname = entry["surname"];
+        people.push_back(person);
+    }
+
+    for (const auto& person : people) {
+        cout << "Name: " << person.name << ", Surname: " << person.surname << endl;
+    }
+
+}
